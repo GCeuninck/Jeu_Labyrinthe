7814f08097a21750f1cd46c01381df3f00bc115c
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2020-01-28 03:10:27 +0100

    Test JUnit Cartes OK

---
+++ Labyrinthe/src/com/project/labyrithe/JUnis_TestTuile.java
@@ -0,0 +1,14 @@
+package com.project.labyrithe;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnis_TestTuile {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Labyrinthe/src/com/project/labyrithe/JUnit_TestCartes.java
@@ -0,0 +1,27 @@
+package com.project.labyrithe;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit_TestCartes {
+
+	@Test
+	void TestInitialiserCartes(){
+
+		Cartes cartes = new Cartes();
+		
+		assertEquals(24, cartes.liste_cartes.size());
+		assertTrue(cartes.liste_cartes.getFirst()[0].equals("Araignee"));
+	}
+	
+	@Test
+	void TestGetCarte() {
+		
+		Cartes cartes = new Cartes();
+		
+		String res = cartes.getCarte();
+		
+		assertEquals(23, cartes.liste_cartes.size());
+	}
+}
